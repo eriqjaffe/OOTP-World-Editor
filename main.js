7ba1b6e94f6bcc4fcfd877726c5bd4026bcca6c0
Eriq Jaffe <eriqjaffe@gmail.com>
2023-07-03 09:31:05 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,211 @@
+const { app, BrowserWindow, dialog, Menu, shell, ipcMain  } = require('electron')
+
+const isMac = process.platform === 'darwin'
+
+ipcMain.on('open-xml', (event, arg) => {
+    const options = {
+		properties: ['openFile'],
+		filters: [
+			{ name: 'XML Files', extensions: ['xml'] }
+		]
+	}
+    dialog.showOpenDialog(null, options).then(result => {
+        if (!result.canceled) {
+            event.sender.send('mainprocess-response', result.filePaths[0])
+        } else {
+            event.sender.send('mainprocess-response', 'cancelled')
+        }
+    })
+    
+});
+
+function createWindow () {
+    const mainWindow = new BrowserWindow({
+      width: 1400,
+      height: 1020,
+      icon: (__dirname + '/images/icon.ico'),
+      webPreferences: {
+          nodeIntegration: true,
+            contextIsolation: false 
+      }
+    })
+    
+    const template = [
+      ...(isMac ? [{
+          label: app.name,
+          submenu: [
+          { role: 'about' },
+          { type: 'separator' },
+          { role: 'services' },
+          { type: 'separator' },
+          { role: 'hide' },
+          { role: 'hideOthers' },
+          { role: 'unhide' },
+          { type: 'separator' },
+          { role: 'quit' }
+          ]
+      }] : []),
+      {
+          label: 'File',
+          submenu: [
+          {
+              click: () => mainWindow.webContents.send('load-uniform','click'),
+              accelerator: isMac ? 'Cmd+L' : 'Control+L',
+              label: 'Load Uniform',
+          },
+		  { type: 'separator' },
+          {
+              click: () => mainWindow.webContents.send('save-uniform','click'),
+              accelerator: isMac ? 'Cmd+S' : 'Control+S',
+              label: 'Save Uniform',
+          },
+          {
+              click: () => mainWindow.webContents.send('save-cap','click'),
+              accelerator: isMac ? 'Cmd+B' : 'Control+B',
+              label: 'Save Cap Only',
+          },
+          {
+              click: () => mainWindow.webContents.send('save-pants','click'),
+              accelerator: isMac ? 'Cmd+P' : 'Control+P',
+              label: 'Save Pants Only',
+          },
+          {
+              click: () => mainWindow.webContents.send('save-jersey','click'),
+              accelerator: isMac ? 'Cmd+J' : 'Control+J',
+              label: 'Save Jersey Only',
+          },
+          {
+              click: () => mainWindow.webContents.send('save-font','click'),
+              accelerator: isMac ? 'Cmd+F' : 'Control+F',
+              label: 'Save Font Only',
+          },
+		  { type: 'separator' },
+          {
+              click: () => mainWindow.webContents.send('save-swatches','click'),
+              accelerator: isMac ? 'Cmd+Shift+S' : 'Control+Shift+S',
+              label: 'Save Palette',
+          },
+          {
+              click: () => mainWindow.webContents.send('load-swatches','click'),
+              accelerator: isMac ? 'Cmd+Shift+L' : 'Control+Shift+L',
+              label: 'Load Palette',
+          },
+		  { type: 'separator' },
+          {
+              click: () => mainWindow.webContents.send('updateFonts','click'),
+              label: 'Refresh User Fonts',
+          },
+		  {
+			click: () => mainWindow.webContents.send('openFontFolder','click'),
+			label: 'Open User Fonts Folder',
+		  },
+		  { type: 'separator' },
+          isMac ? { role: 'close' } : { role: 'quit' }
+          ]
+      },
+	  {
+		  label: 'Edit',
+		  submenu: [
+			{
+				click: () => mainWindow.webContents.send('copy','click'),
+				accelerator: isMac ? 'Cmd+C' : 'Control+C',
+				label: 'Copy',
+			},
+			{
+				click: () => mainWindow.webContents.send('paste','click'),
+				accelerator: isMac ? 'Cmd+V' : 'Control+V',
+				label: 'Paste',
+			},
+			{ type: 'separator' },
+			{
+				click: () => mainWindow.webContents.send('prefs','click'),
+				accelerator: isMac ? 'Cmd+Shift+P' : 'Control+Shift+P',
+				label: 'Edit Preferences',
+			}
+		  ]
+	  },
+      {
+          label: 'View',
+          submenu: [
+          { role: 'reload' },
+          { role: 'forceReload' },
+          { role: 'toggleDevTools' },
+          { type: 'separator' },
+          { role: 'resetZoom' },
+          { role: 'zoomin', accelerator: 'CommandOrControl+=' },
+          { role: 'zoomout', accelerator: 'CommandOrControl+-' },
+          { type: 'separator' },
+          { role: 'togglefullscreen' }
+          ]
+      },
+      {
+          label: 'About',
+          submenu: [
+          {
+              click: () => mainWindow.webContents.send('about','click'),
+                  label: 'About the OOTP Uniform Maker',
+          },
+          {
+              label: 'About OOTP Baseball',
+              click: async () => {    
+              await shell.openExternal('https://www.ootpdevelopments.com/out-of-the-park-baseball-home/')
+              }
+          },
+		  { type: 'separator' },
+          {
+              label: 'About Node.js',
+              click: async () => {    
+              await shell.openExternal('https://nodejs.org/en/about/')
+              }
+          },
+          {
+              label: 'About Electron',
+              click: async () => {
+              await shell.openExternal('https://electronjs.org')
+              }
+          },
+		  {
+			  label: 'About fabric.js',
+			  click: async () => {
+			  await shell.openExternal('http://fabricjs.com/')
+			  }
+		  },
+		  { type: 'separator' },
+          {
+              label: 'View project on GitHub',
+              click: async () => {
+              await shell.openExternal('https://github.com/eriqjaffe/OOTP-Uniform-Maker')
+              }
+          },
+		  {
+			  click: () => mainWindow.webContents.send('update','click'),
+			  label: 'Check For Updates',
+		  }
+          ]
+      }
+      ]
+      
+      const menu = Menu.buildFromTemplate(template)
+      Menu.setApplicationMenu(menu)
+  
+    //mainWindow.loadURL(`file://${__dirname}/index.html?port=${server.address().port}&appVersion=${pkg.version}&preferredColorFormat=${preferredColorFormat}&preferredJerseyTexture=${preferredJerseyTexture}&preferredPantsTexture=${preferredPantsTexture}&preferredCapTexture=${preferredCapTexture}&gridsVisible=${gridsVisible}&checkForUpdates=${checkForUpdates}&preferredNameFont=${preferredNameFont}&preferredNumberFont=${preferredNumberFont}&preferredCapFont=${preferredCapFont}&preferredJerseyFont=${preferredJerseyFont}&seamsVisibleOnDiffuse=${seamsVisibleOnDiffuse}&preferredHeightMapBrightness=${preferredHeightMapBrightness}&preferredSeamOpacity=${preferredSeamOpacity}&imagemagick=${imInstalled}`);
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    
+    mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+      shell.openExternal(url);
+      return { action: 'deny' };
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
